3724da08d3945eb013269f863a670e7a8ae87860
lassombra <briatt22@gmail.com>
2025-01-04 13:10:51 -0600

    player renders

--- main.py
+++ main.py
@@ -3,17 +3,24 @@
 # throughout this file
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     while True:
         screen.fill((0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 return
+        dt += clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
